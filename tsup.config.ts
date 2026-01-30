c1b676cf0f24d5a755e39c192fe3c374205d19c1
sambernhardt <sdbernhardt@gmail.com>
2026-01-29 21:41:27 -0500

    Remove sass

--- tsup.config.ts
+++ tsup.config.ts
@@ -8,12 +8,4 @@ export default defineConfig({
   splitting: false,
   sourcemap: true,
   treeshake: true,
-  // Enable CSS support - esbuild automatically handles .module.css files as CSS modules
-  // SCSS files are pre-compiled to CSS via the build:css script
-  esbuildOptions(options) {
-    options.loader = {
-      ...options.loader,
-      ".css": "css",
-    };
-  },
 });
